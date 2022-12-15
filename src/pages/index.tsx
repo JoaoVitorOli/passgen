ac37932c4c952f867d0e5fd520305afc38334e4d
João Vitor <infamoz_br3@hotmail.com>
2022-12-15 12:00:27 -0300

    rest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import Head from 'next/head'
+import { useEffect } from 'react';
 import { GeneratedPassword } from '../components/GeneratedPassword';
 import { PasswordConfig } from '../components/PasswordConfig';
 
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
+  useEffect(() => {
+    async function doFetch() {
+      const response = await fetch('http://34.75.33.161/wp-json/api/devs', {
+        method: 'get'
+      });
+
+      if (response.ok) {
+        console.log('success:');
+        console.log(response);
+      } else {
+        console.log('error:');
+        console.log(response);
+      }
+    }
+
+
+    doFetch();
+  });
+
   return (
     <div>
       <Head>
