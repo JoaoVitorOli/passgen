9936f97e9869b023397782973e3faf5558b73903
João Vitor <joao.vitor012020@gmail.com>
2023-04-15 17:42:34 -0300

    wtf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,10 @@
 import Head from 'next/head'
-import { useEffect } from 'react';
 import { GeneratedPassword } from '../components/GeneratedPassword';
 import { PasswordConfig } from '../components/PasswordConfig';
 
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
-  useEffect(() => {
-    async function doFetch() {
-      const response = await fetch('https://dev-essential.pecege.com/wp-json/api/devs', {
-        method: 'get'
-      });
-
-      if (response.ok) {
-        console.log('success:');
-        console.log(response);
-      } else {
-        console.log('error:');
-        console.log(response);
-      }
-    }
-
-
-    doFetch();
-  });
-
   return (
     <div>
       <Head>
